1fef1797a5bda7c4db5155a901722b0a27a3bd46
Bay Yang Jie <153354426+bayyangjie@users.noreply.github.com>
2025-03-24 19:24:00 +0800

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -17,3 +17,92 @@ Usage Notes:
 =====================================================================================
 */
 
+-- ==================================================================================
+-- Checking 'silver.crm_cust_info'
+-- ==================================================================================
+-- Check for NULLs or Duplicates in Primary Key
+SELECT
+cst_id,
+COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 or cst_id IS NULL  /* 'or cst_id IS NULL' is in case 'cst_id' happens to have only one NULL record, it should also be considered */
+
+
+--- Check and Remove unwanted spaces in the columns with string values (e.g cst_firstname, cst_lastname, cst_marital_status, cst_gndr) to ensure data consistency and uniformity
+/* if the original value is not equal to the value after trimming, it means there are spaces in the original value */
+SELECT cst_gndr
+FROM silver.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr)
+
+
+--- Data Standardization & Consistency. Checking the data levels in columns with low cardinalty.
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+
+-- ==================================================================================
+-- Checking 'silver.crm_prd_info'
+-- ==================================================================================
+-- Check for NULLs or Duplicates in Primary Key
+SELECT
+    prd_id,
+    COUNT(*) 
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+
+--- Check for unwanted spaces in string values (prd_nm, prd_line, prd_key, cat_id)
+/* if the original value is not equal to the value after trimming, it means there are spaces in the original value */
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+
+--- Check for NULLs or negative numbers
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+--- Data Standardization & Consistency. Checking the data levels in columns with low cardinalty.
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+
+-- Check for invalid date orders
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+
+-- ==================================================================================
+-- Checking 'silver.crm_sales_details'
+-- ==================================================================================
+-- Check for Invalid Dates
+/* check returns clean with no invalid date orders */
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+
+-- Check data consistency between Sales, Quantity and Price after the transformations were applied previously
+/* check shows that calculations tally between the sales, quantity and price variables and the values format are consistent (i.e non NULLs, non-zeros) */
+
+sls_sales,
+sls_quantity, 
+sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price    
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL   
+OR sls_sales <=0 OR sls_quantity <=0 OR sls_price <=0   
+ORDER BY sls_sales, sls_quantity, sls_price;
+
+
+-- ==================================================================================
+-- Checking 'silver.erp_cust_az12'
+-- ==================================================================================
+-- Identify Out-of-Range Dates
