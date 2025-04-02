bff8c9cd1548bf346b1a5dd41d0be7153339a76c
Bay Yang Jie <153354426+bayyangjie@users.noreply.github.com>
2025-04-02 22:55:28 +0800

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -26,7 +26,7 @@ GO
 
 CREATE VIEW gold.dim_customers AS
 SELECT      
-	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,   <!-- The surrogate keys just assigns incrementing serial numbers to the rows even if there are duplicated rows. Cnt use the surrogate key in the 'ON' clause as it's not considered an existing surrogate key -->
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,   -- The surrogate keys just assign incrementing serial numbers to the rows even if there are duplicated rows. Cnt use the surrogate key in the 'ON' clause as it's not considered an existing surrogate key
 	ci.cst_id AS customer_id, 
 	ci.cst_key AS customer_number,
 	ci.cst_firstname AS first_name,
@@ -43,3 +43,60 @@ LEFT JOIN silver.erp_CUST_AZ12 ca
 ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_LOC_A101 la
 ON ci.cst_key = la.cid;
+
+
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
+
+CREATE VIEW gold.dim_products AS
+SELECT
+	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,    -- The surrogate keys just assigns incrementing serial numbers to the rows even if there are duplicated rows. Cannot use in this same query in 'ON' statement as it is not considered existing surrogate key.
+	/* 
+	How ROW_NUMBER() statement works:
+	MUST have either a ORDER BY or PARTITION BY to tell SQL how the number is going to be done. 
+	Order by start date then for products with same start date, sort those by product key 
+	*/
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance,
+	pn.prd_cost AS cost, 
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_PX_CAT_G1V2 pc
+ON pn.cat_id = pc.id     -- recall that 'cat_id' was created by splitting out from the 'prd_key' column during the transformation step
+WHERE prd_end_dt IS NULL;  -- to filter out historical data and only return current ongoing data for each . can also exclude 'end_dt' column in this case
+
+
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num  AS order_number,
+    pr.product_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+    ON sd.sls_cust_id = cu.customer_id;
+GO
