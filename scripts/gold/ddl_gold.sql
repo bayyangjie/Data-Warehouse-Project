969e9fe04c981148de70758ee8e14231d60e317a
Bay Yang Jie <153354426+bayyangjie@users.noreply.github.com>
2025-04-02 23:13:59 +0800

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -21,7 +21,7 @@ Usage:
 -- Create Dimension: gold.dim_customers
 -- ================================================================================================
 IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
-    DROP VIEW gold.dim_customers;        -- Drop view if exists then recreate it agian with CREATE VIEW
+    DROP VIEW gold.dim_customers;        -- Drop view if exists then recreate it agian with CREATE VIEW. This prevents data duplication when script is ran more than once.
 GO
 
 CREATE VIEW gold.dim_customers AS
